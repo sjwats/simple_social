9d176ce35410376cd232a0e492c7b00bd3283af8
John <s.johnwatson@gmail.com>
2013-12-29 12:05:43 -0500

    First commit, setting up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleSocial::Application.config.secret_key_base = 'c3698d1e2d6c42cf123a02c58a00461df163b1827d07939527fb9cf1f22c33c520987fd362a668b0518b56bad370899f11c1cb8c93799b6949479dcdc080fcde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleSocial::Application.config.session_store :cookie_store, key: '_simple_social_session'
