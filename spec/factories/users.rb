9ad9eb955729d4251c1ba7aad59ac53c520c47be
John <s.johnwatson@gmail.com>
2014-01-02 12:55:38 -0500

    reauthentication feature and tests complete

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
+    first_name 'El'
+    last_name 'Presidente'
+    sequence(:email) {|n| "the#{n}@president.com" }
+    password 'password'
+    password_confirmation 'password'
   end
 end
