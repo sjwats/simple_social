2b05034d2db08e6adce80f3d700ec88093b0dfad
John <s.johnwatson@gmail.com>
2014-01-23 17:31:52 -0500

    changed home page to the pages index and added method in app controller to redirect to users index page upon sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,9 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    users_path
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SimpleSocial::Application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'pages#index'
   resources :pages, only: [:index]
   resources :users do
     resources :activities
