aad561a6eb7603ae5caa5cc801603faf7615e632
John <s.johnwatson@gmail.com>
2014-01-24 09:13:22 -0500

    sendgrid configuration testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119234051) do
+ActiveRecord::Schema.define(version: 20140124134754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: true do |t|
     t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "attendees", force: true do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "event_id",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
