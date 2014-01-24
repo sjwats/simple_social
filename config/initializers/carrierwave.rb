348cb2a88e4a3ad02e49be605e176f3673e0c4d9
John <s.johnwatson@gmail.com>
2014-01-24 12:25:13 -0500

    changed fog directory in carrierwave intiializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     aws_access_key_id: ENV['AWS_ACCESS_KEY'],
     aws_secret_access_key: ENV['AWS_SECRET_KEY']
   }
-  config.fog_directory  = "simple-social-#{Rails.env}"
+  config.fog_directory  = "simple-social"
 end
