3da98b71170473da9ac3dcbe57194c24b9eef3bc
John <s.johnwatson@gmail.com>
2014-01-27 11:00:02 -0500

    uploads working in production now

--- spec/features/post_on_wall_spec.rb
+++ spec/features/post_on_wall_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-feature "user can post on another users wall" do
-  scenario 'user posts on another users wall' do
+feature "user can post on a user show page" do
+  scenario 'user posts on their user show page' do
     user = FactoryGirl.create(:user)
     foosball = Activity.create(name: 'Foosball')
     running = Activity.create(name: 'Running')
